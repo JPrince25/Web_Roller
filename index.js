5c4691b237e44c3db53f77035fb91e59c468ba56
Jacob Prince <jacobmprince@lewisu.edu>
2023-02-06 00:24:14 -0600

    Change

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // Roll Command
 function roll() {
     prob = Math.random()*6
-    //Head Dice
+    //Head Die
     if (prob < 3){
         document.getElementById("dice").src="images/bobRossLogo.png";
-    //Card Dice
+    //Card Die
     } else if (prob < 4){
         document.getElementById("dice").src="images/cards.png";
-    //Hand Dice
+    //Hand Die
     } else if (prob < 5){
         document.getElementById("dice").src="images/hand.png";
-    //Palette Dice
+    //Palette Die
     } else if (prob < 6){
         document.getElementById("dice").src="images/palette.png";
     }
 } 
 
+// Roll Die on "Enter"
 var enterInput = document.getElementById("roll");
 enterInput.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
